57a27a99737e8c1aff71df1a35be00befccff643
Robert Cope <robert.cope@pushrodtechnology.com>
2015-04-19 21:05:29 -0600

    Basic data read added to Linux.

--- OpenScopeTools.py
+++ OpenScopeTools.py
@@ -11,6 +11,13 @@ class Oscilloscope(object):
     VENDOR_ID = 0x04B5
     MODEL_ID = 0x6022
 
+    UPLOAD_FIRMWARE_REQUEST = 0xa0
+    UPLOAD_FIRMWARE_INDEX = 0x00
+
+    READ_CALIBRATION_REQUEST = 0xa2
+    READ_CALIBRATION_VALUE = 0x08
+    READ_CALIBRATION_INDEX = 0x00
+
     def __init__(self, scope_id=0):
         self.device = None
         self.device_handle = None
@@ -38,10 +45,11 @@ class Oscilloscope(object):
         self.device_handle.claimInterface(0)
         return True
 
-    def close_handle(self):
+    def close_handle(self, release_interface=True):
         if not self.device_handle:
             return True
-        self.device_handle.releaseInterface(0)
+        if release_interface:
+            self.device_handle.releaseInterface(0)
         self.device_handle.close()
         self.device_handle = None
         return True
@@ -49,15 +57,17 @@ class Oscilloscope(object):
     def __del__(self):
         self.close_handle()
 
-    def flash_firmware(self):
+    def flash_firmware(self, firmware=device_firmware):
         if not self.device_handle:
             assert self.open_handle()
-        for packet in device_firmware:
-            bytes_written = self.device_handle.controlWrite(0x40, 0xA0, packet.value, 0x00, packet.data, 0x00)
+        for packet in firmware:
+            bytes_written = self.device_handle.controlWrite(0x40, self.UPLOAD_FIRMWARE_REQUEST,
+                                                            packet.value, self.UPLOAD_FIRMWARE_INDEX,
+                                                            packet.data, timeout=60)
             assert bytes_written == packet.size
         # After firmware is written, scope will typically show up again as a different device, so scan again
         time.sleep(0.1)
-        self.close_handle()
+        self.close_handle(release_interface=False)
         self.setup()
         self.open_handle()
         return True
@@ -65,5 +75,13 @@ class Oscilloscope(object):
     def get_calibration_values(self):
         if not self.device_handle:
             assert self.open_handle()
-        return self.device_handle.controlRead(0x40, 0xA2, 0x08, 0x00, 0x10, 30)
+        return self.device_handle.controlRead(0x40, self.READ_CALIBRATION_REQUEST, self.READ_CALIBRATION_VALUE,
+                                              self.READ_CALIBRATION_INDEX, 0x20)
 
+    def read_data(self, data_size=0x400):
+        data_size <<= 0x1
+        if not self.device_handle:
+            assert self.open_handle()
+        self.device_handle.controlRead(0x40, 0xe3, 0x00, 0x00, 0x01)
+        data = self.device_handle.bulkRead(0x86, data_size)
+        return data[::2], data[1::2]
--- OpenScopeToolsTest.py
+++ OpenScopeToolsTest.py
@@ -26,3 +26,29 @@ class BasicTests(TestCase):
         print map(ord, cal_values)
         assert cal_values
         assert scope.close_handle()
+
+    def test_read_data(self):
+        scope = Oscilloscope()
+        assert scope.setup()
+        assert scope.open_handle()
+        ch1_data, _ = scope.read_data(data_size=0x400)
+        ch1_data = map(ord, ch1_data)
+        print ch1_data
+        assert ch1_data
+        assert scope.close_handle()
+
+    def test_read_many_sizes(self):
+        scope = Oscilloscope()
+        assert scope.setup()
+        assert scope.open_handle()
+        data_size = 0x400
+        for _ in xrange(11):
+            print "DATA SIZE", data_size
+            ch1_data, ch2_data = scope.read_data(data_size=data_size)
+            print len(ch1_data)
+            # print map(ord, ch1_data)
+            print len(ch2_data)
+            # print map(ord, ch2_data)
+            assert ch1_data, ch2_data
+            data_size <<= 1
+        assert scope.close_handle()
