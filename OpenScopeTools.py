a7d4bd99f005a6d1fff5e8b94ab6c7d549a38202
Robert Cope <robert.cope@pushrodtechnology.com>
2015-04-19 22:33:40 -0600

    It looks like most (all?) of the SDK calls are mapped to libusb calls, so there is a pretty full Linux prototype here. Should address issue #3.

--- OpenScopeTools.py
+++ OpenScopeTools.py
@@ -14,9 +14,55 @@ class Oscilloscope(object):
     UPLOAD_FIRMWARE_REQUEST = 0xa0
     UPLOAD_FIRMWARE_INDEX = 0x00
 
-    READ_CALIBRATION_REQUEST = 0xa2
-    READ_CALIBRATION_VALUE = 0x08
-    READ_CALIBRATION_INDEX = 0x00
+    RW_CALIBRATION_REQUEST = 0xa2
+    RW_CALIBRATION_VALUE = 0x08
+    RW_CALIBRATION_INDEX = 0x00
+
+    SET_SAMPLE_RATE_REQUEST = 0xe2
+    SET_SAMPLE_RATE_VALUE = 0x00
+    SET_SAMPLE_RATE_INDEX = 0x00
+
+    SET_CH1_VR_REQUEST = 0xe0
+    SET_CH1_VR_VALUE = 0x00
+    SET_CH1_VR_INDEX = 0x00
+
+    SET_CH2_VR_REQUEST = 0xe1
+    SET_CH2_VR_VALUE = 0x00
+    SET_CH2_VR_INDEX = 0x00
+
+    SAMPLE_RATES = {0: ("48 MS/s", 48e6),
+                    1: ("48 MS/s", 48e6),
+                    2: ("48 MS/s", 48e6),
+                    3: ("48 MS/s", 48e6),
+                    4: ("48 MS/s", 48e6),
+                    5: ("48 MS/s", 48e6),
+                    6: ("48 MS/s", 48e6),
+                    7: ("48 MS/s", 48e6),
+                    8: ("48 MS/s", 48e6),
+                    9: ("48 MS/s", 48e6),
+                    10: ("48 MS/s", 48e6),
+                    11: ("16 MSa/s", 16e6),
+                    12: ("8 MSa/s", 8e6),
+                    13: ("4 MSa/s", 4e6),
+                    14: ("1 MS/s", 1e6),
+                    15: ("1 MS/s", 1e6),
+                    16: ("1 MS/s", 1e6),
+                    17: ("1 MS/s", 1e6),
+                    18: ("1 MS/s", 1e6),
+                    19: ("1 MS/s", 1e6),
+                    20: ("1 MS/s", 1e6),
+                    21: ("1 MS/s", 1e6),
+                    22: ("1 MS/s", 1e6),
+                    23: ("1 MS/s", 1e6),
+                    24: ("1 MS/s", 1e6),
+                    25: ("500 KSa/s", 500e3),
+                    26: ("200 KSa/s", 200e3),
+                    27: ("100 KSa/s", 100e3)}
+
+    VOLTAGE_RANGES = {0x01: ('+/- 5V', 0.0390625, 2.5),
+                      0x02: ('+/- 2.5V', 0.01953125, 1.25),
+                      0x05: ('+/- 1V', 0.0078125, 0.5),
+                      0x0a: ('+/- 500mV', 0.00390625, 0.25)}
 
     def __init__(self, scope_id=0):
         self.device = None
@@ -57,13 +103,13 @@ class Oscilloscope(object):
     def __del__(self):
         self.close_handle()
 
-    def flash_firmware(self, firmware=device_firmware):
+    def flash_firmware(self, firmware=device_firmware, timeout=60):
         if not self.device_handle:
             assert self.open_handle()
         for packet in firmware:
             bytes_written = self.device_handle.controlWrite(0x40, self.UPLOAD_FIRMWARE_REQUEST,
                                                             packet.value, self.UPLOAD_FIRMWARE_INDEX,
-                                                            packet.data, timeout=60)
+                                                            packet.data, timeout=timeout)
             assert bytes_written == packet.size
         # After firmware is written, scope will typically show up again as a different device, so scan again
         time.sleep(0.1)
@@ -72,16 +118,58 @@ class Oscilloscope(object):
         self.open_handle()
         return True
 
-    def get_calibration_values(self):
+    def get_calibration_values(self, timeout=0):
         if not self.device_handle:
             assert self.open_handle()
-        return self.device_handle.controlRead(0x40, self.READ_CALIBRATION_REQUEST, self.READ_CALIBRATION_VALUE,
-                                              self.READ_CALIBRATION_INDEX, 0x20)
+        return self.device_handle.controlRead(0x40, self.RW_CALIBRATION_REQUEST, self.RW_CALIBRATION_VALUE,
+                                              self.RW_CALIBRATION_INDEX, 0x20, timeout=timeout)
 
-    def read_data(self, data_size=0x400):
+    def set_calibration_values(self, cal_list, timeout=0):
+        if not self.device_handle:
+            assert self.open_handle()
+        cal_list = cal_list if isinstance(cal_list, basestring) else "".join(map(chr, cal_list))
+        return self.device_handle.controlRead(0x40, self.RW_CALIBRATION_REQUEST, self.RW_CALIBRATION_VALUE,
+                                              self.RW_CALIBRATION_INDEX, cal_list, timeout=timeout)
+
+    def read_data(self, data_size=0x400, raw=False, timeout=0):
         data_size <<= 0x1
         if not self.device_handle:
             assert self.open_handle()
-        self.device_handle.controlRead(0x40, 0xe3, 0x00, 0x00, 0x01)
-        data = self.device_handle.bulkRead(0x86, data_size)
-        return data[::2], data[1::2]
+        self.device_handle.controlRead(0x40, 0xe3, 0x00, 0x00, 0x01, timeout=timeout)
+        data = self.device_handle.bulkRead(0x86, data_size, timeout=timeout)
+        if raw:
+            return data[::2], data[1::2]
+        else:
+            return map(ord, data[::2]), map(ord, data[1::2])
+
+    @staticmethod
+    def scale_read_data(read_data, voltage_range):
+        scale_factor = 5.0/(voltage_range << 7)
+        return [(datum - 128)*scale_factor for datum in read_data]
+
+    def set_sample_rate(self, rate_index, timeout=0):
+        if not self.device_handle:
+            assert self.open_handle()
+        bytes_written = self.device_handle.controlWrite(0x40, self.SET_SAMPLE_RATE_REQUEST,
+                                                        self.SET_SAMPLE_RATE_VALUE, self.SET_SAMPLE_RATE_INDEX,
+                                                        chr(rate_index), timeout=timeout)
+        assert bytes_written == 0x01
+        return True
+
+    def set_ch1_voltage_range(self, range_index, timeout=0):
+        if not self.device_handle:
+            assert self.open_handle()
+        bytes_written = self.device_handle.controlWrite(0x40, self.SET_CH1_VR_REQUEST,
+                                                        self.SET_CH1_VR_VALUE, self.SET_CH1_VR_INDEX,
+                                                        chr(range_index), timeout=timeout)
+        assert bytes_written == 0x01
+        return True
+
+    def set_ch2_voltage_range(self, range_index, timeout=0):
+        if not self.device_handle:
+            assert self.open_handle()
+        bytes_written = self.device_handle.controlWrite(0x40, self.SET_CH2_VR_REQUEST,
+                                                        self.SET_CH2_VR_VALUE, self.SET_CH2_VR_INDEX,
+                                                        chr(range_index), timeout=timeout)
+        assert bytes_written == 0x01
+        return True
--- OpenScopeToolsTest.py
+++ OpenScopeToolsTest.py
@@ -32,7 +32,6 @@ class BasicTests(TestCase):
         assert scope.setup()
         assert scope.open_handle()
         ch1_data, _ = scope.read_data(data_size=0x400)
-        ch1_data = map(ord, ch1_data)
         print ch1_data
         assert ch1_data
         assert scope.close_handle()
@@ -44,11 +43,39 @@ class BasicTests(TestCase):
         data_size = 0x400
         for _ in xrange(11):
             print "DATA SIZE", data_size
-            ch1_data, ch2_data = scope.read_data(data_size=data_size)
+            ch1_data, ch2_data = scope.read_data(data_size=data_size, raw=True)
             print len(ch1_data)
-            # print map(ord, ch1_data)
             print len(ch2_data)
-            # print map(ord, ch2_data)
             assert ch1_data, ch2_data
             data_size <<= 1
+        assert scope.close_handle()
+
+    def test_set_sample_rate(self):
+        scope = Oscilloscope()
+        assert scope.setup()
+        assert scope.open_handle()
+        for rate_index in scope.SAMPLE_RATES.keys():
+            scope.set_sample_rate(rate_index)
+        assert scope.close_handle()
+
+    def test_set_channel_voltage_range(self):
+        scope = Oscilloscope()
+        assert scope.setup()
+        assert scope.open_handle()
+        for vrange in scope.VOLTAGE_RANGES.keys():
+            assert scope.set_ch1_voltage_range(vrange)
+            assert scope.set_ch1_voltage_range(vrange)
+        assert scope.close_handle()
+
+    def test_data_scaling(self):
+        scale_factor = 0x0a
+        scope = Oscilloscope()
+        assert scope.setup()
+        assert scope.open_handle()
+        assert scope.set_ch1_voltage_range(scale_factor)
+        assert scope.set_sample_rate(27)
+        ch1_data, _ = scope.read_data(0x100000)
+        ch1_data = scope.scale_read_data(ch1_data, scale_factor)
+        print "Max:", max(ch1_data), "(V), Min:", min(ch1_data), "(V)"
+        assert ch1_data
         assert scope.close_handle()
